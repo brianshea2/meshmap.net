91a48310bdb0af4fa4c9fddab8e62ca732764558
root <165865819+brianshea2@users.noreply.github.com>
2025-02-21 22:00:21 +0000

    bring back blocklist

--- scripts/run-meshobserv.sh
+++ scripts/run-meshobserv.sh
@@ -7,4 +7,5 @@ docker run --name meshobserv \
     --restart unless-stopped \
     -v /data:/data \
     -d meshobserv \
+    -b /data/meshmap.net/blocklist.txt \
     -f /data/meshmap.net/website/nodes.json
