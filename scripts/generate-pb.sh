c67afd0a6b373598d5635df0e09f98a6edd07d5f
root <165865819+brianshea2@users.noreply.github.com>
2025-08-11 15:01:24 +0000

    update protobuf compilation

--- scripts/generate-pb.sh
+++ scripts/generate-pb.sh
@@ -6,11 +6,15 @@ docker run \
     golang \
     bash -c "
         apt-get update &&
-        apt-get install -y protobuf-compiler patch &&
+        apt-get install -y protobuf-compiler &&
         go install google.golang.org/protobuf/cmd/protoc-gen-go@latest &&
         cd /tmp &&
         git clone --progress --depth 1 https://github.com/meshtastic/protobufs.git &&
-        patch -p1 < /data/patch/remove-nanopb.patch &&
         rm -rf /data/internal/meshtastic/generated &&
-        protoc -I=protobufs --go_out=/data/internal/meshtastic --go_opt=module=github.com/meshtastic/go protobufs/meshtastic/*.proto
+        protoc \\
+            -I=protobufs \\
+            --go_out=/data/internal/meshtastic \\
+            --go_opt=module=github.com/meshtastic/go \\
+            protobufs/nanopb.proto \\
+            protobufs/meshtastic/*.proto
     "
